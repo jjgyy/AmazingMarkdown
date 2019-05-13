3c95643eaa41faee4cd9b3367526e8f24e148be1
jjgyy <jjgyy@126.com>
2019-05-13 17:52:39 +0800

    initial

---
+++ AmazingMarkdown/Controllers/AMCollationSettingTableController.h
@@ -0,0 +1,17 @@
+//
+//  AMCollationSettingTableController.h
+//  AmazingMarkdown
+//
+//  Created by Apple on 2019/5/13.
+//  Copyright © 2019 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AMCollationSettingTableController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
